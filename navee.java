7d2777ad6e08e7b1b87ddc54118db2b04028ed91
navenaveetha25 <47380133+navenaveetha25@users.noreply.github.com>
2019-02-06 13:43:58 +0530

    Create navee.java

---
+++ navee.java
@@ -0,0 +1,19 @@
+public class positive
+{
+      public static void main(String[] args) 
+    {
+        int number=-5;
+        if(number > 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+    }
+}
