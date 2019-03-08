a93e40b5a3b9b5f57723a7c47a11ff036c65ecb9
navenaveetha25 <47380133+navenaveetha25@users.noreply.github.com>
2019-03-08 11:50:35 +0530

    Update and rename oddorevent.java to EvenOdd .java

---
+++ EvenOdd .java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class EvenOdd {
+
+    public static void main(String[] args)
+    {
+
+        Scanner reader = new Scanner(System.in);
+        int num = reader.nextInt();
+        if(num>0)
+        {  
+        if(num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+        }
+        else
+           System.out.println("Invalid"); 
+    }
+}
